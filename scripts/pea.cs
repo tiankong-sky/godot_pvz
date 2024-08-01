e1d14b3035f235bc9b999a1c1d67c2d16af819b5
tiankong <951545075@qq.com>
2024-08-01 21:34:26 +1000

    try to create pea

--- scripts/pea.cs
+++ scripts/pea.cs
@@ -4,6 +4,8 @@ using System;
 public partial class pea : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
+	private double speed = 100;
+	private Vector2 dir = Vector2.Left;
 	public override void _Ready()
 	{
 	}
