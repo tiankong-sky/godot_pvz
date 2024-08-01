5ce116a96484ea963592710ebd3edfa5951acc74
tiankong <951545075@qq.com>
2024-08-01 21:51:53 +1000

    add to background

--- scripts/pea.cs
+++ scripts/pea.cs
@@ -4,8 +4,8 @@ using System;
 public partial class Pea : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
-	private double speed = 100;
-	private Vector2 dir = Vector2.Left;
+	private double _speed = 100;
+	private Vector2 dir = Vector2.Right;
 	public override void _Ready()
 	{
 	}
@@ -13,5 +13,7 @@ public partial class Pea : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position += (float)(_speed * delta) * dir;
 	}
+
 }
